705874e2ee8c0366242e158035a2e16a8a3762d5
Shahid Hassan <149702259+Markhor072@users.noreply.github.com>
2025-08-27 03:52:20 +0300

    Update sanityClient.js

--- src/lib/sanityClient.js
+++ src/lib/sanityClient.js
@@ -1,8 +1,9 @@
-import { createClient } from '@sanity/client'
+// import { createClient } from '@sanity/client'
 
-export const client = createClient({
-  projectId: '69tpz6ep',   // 🔹 replace with your real projectId from sanity.config.js
-  dataset: 'production',   // 🔹 same dataset you selected
-  apiVersion: '2025-08-22', // 🔹 today’s date ensures the latest schema
-  useCdn: true,             // 🔹 true = faster, cached responses (set false if you need real-time)
-})
+// export const client = createClient({
+//   projectId: '69tpz6ep',   // 🔹 replace with your real projectId from sanity.config.js
+//   dataset: 'production',   // 🔹 same dataset you selected
+//   apiVersion: '2025-08-22', // 🔹 today’s date ensures the latest schema
+//   useCdn: true,             // 🔹 true = faster, cached responses (set false if you need real-time)
+// })
+
